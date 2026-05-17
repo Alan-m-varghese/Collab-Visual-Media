a9d1332eec32b8a197414ee174b61a09486faa52
Alan M Varghese <alanmvarghese123@gmail.com>
2026-05-18 00:06:04 +0530

    Delete update_html_bright.py

--- update_html_bright.py
+++
@@ -1,7 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('src="assets/logos_color_fixed/', 'src="assets/logos_color_bright/')
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(content)
