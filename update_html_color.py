dc5f564c0edef7ffec297817c3ed4eea1b7bdd92
Alan M Varghese <alanmvarghese123@gmail.com>
2026-05-18 00:05:50 +0530

    Delete update_html_color.py

--- update_html_color.py
+++
@@ -1,9 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('src="assets/logos/', 'src="assets/logos_color/')
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print('Updated index.html to use logos_color')
